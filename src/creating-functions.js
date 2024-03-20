5a42e22ec4b24a89c570aad4214b1a10d4bf2987
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-20 22:57:43 +0100

    added creating functions

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -25,11 +25,12 @@ console.log(addOneToNumber(1))
 // TODO: write code below
 
 function stringToUppercase(myString) {
-  return myString.toUpperCase()
+  const firstLetter = myString[0].toUpperCase()
+  const restString = myString.slice(1)
+  const result = firstLetter + restString
+  return result
 }
-console.log(stringToUppercase('mahBallz'))
-
-// only first letter?
+console.log(stringToUppercase('teST'))
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -41,6 +42,14 @@ console.log(stringToUppercase('mahBallz'))
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+function greetName(name) {
+  const firstLetter = name[0].toUpperCase()
+  const restName = name.slice(1)
+  const result = 'Hi, ' + firstLetter + restName + ' :)'
+  return result
+}
+console.log(greetName('rebecca'))
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -54,6 +63,19 @@ console.log(stringToUppercase('mahBallz'))
 //
 // TODO: write code below
 
+function countStringInArray(myArray) {
+  let stringCounter = 0
+  for (let i = 0; i < myArray.length; i++) {
+    if (typeof myArray[i] !== 'string') {
+      continue
+    } else {
+      stringCounter += 1
+    }
+  }
+  return stringCounter
+}
+console.log(countStringInArray([1, 'robert', 'rebecca', 'test', 3, '4']))
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -65,11 +87,20 @@ console.log(stringToUppercase('mahBallz'))
 //
 // TODO: write code below
 
+function addKeyAmazingEdward(myObject) {
+  if (!('edward' in myObject)) {
+    myObject.edward = 'amazing'
+  }
+  return myObject
+}
+
+console.log(addKeyAmazingEdward({ edward: 'fab' }))
+
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
   a: addOneToNumber, // 1. change undefined to be the name of the function you defined for the first TODO
   b: stringToUppercase, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  c: greetName, // etc
+  d: countStringInArray,
+  e: addKeyAmazingEdward
 }
