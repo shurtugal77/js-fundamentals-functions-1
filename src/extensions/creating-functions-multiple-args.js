f85ecd3039e87b1e4d3579bf69310900c064ea01
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-21 23:56:31 +0100

    added extension: functions with multiple arguments

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -10,6 +10,15 @@
 //
 // TODO: write code below
 
+function numberSeries(lowerNumber, upperNumber) {
+  const result = []
+  for (let i = lowerNumber; i <= upperNumber; i++) {
+    result.push(i)
+  }
+  return result
+}
+console.log(numberSeries(-5, 20))
+
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,6 +30,15 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+
+function uppercaseExclamation(myString, myNumber) {
+  myString = myString.toUpperCase()
+  for (let i = 0; i < myNumber; i++) {
+    myString += '!'
+  }
+  return myString
+}
+console.log(uppercaseExclamation('nuesse', 12))
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -34,9 +52,44 @@
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+function increaseTimeByMinute(currentTime, addMinutes) {
+  const time = currentTime.split(':')
+  const hours = Number(time[0])
+  const minutes = Number(time[1])
+  let newMinutes = minutes + addMinutes
+  let addHours = 0
+  let newHours = 0
+  let newTime = ''
+
+  if (newMinutes > 59) {
+    addHours = Math.floor(newMinutes / 60)
+    newMinutes = newMinutes % 60
+  }
+
+  newHours = hours + addHours
+  if (newHours > 23) {
+    while (newHours > 23) {
+      newHours = newHours - 24
+    }
+  }
+
+  if (newHours === 0) {
+    newHours = '0' + newHours.toString()
+  }
+
+  if (newMinutes < 10) {
+    newMinutes = '0' + newMinutes.toString()
+  }
+
+  newTime = newHours.toString() + ':' + newMinutes.toString()
+  return newTime
+}
+
+console.log(increaseTimeByMinute('23:50', 30))
+
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: numberSeries, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: uppercaseExclamation, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: increaseTimeByMinute // etc
 }
